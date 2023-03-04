ac2863a364cd97bcb76791bbf82a75476c2ed68b
Ra Wai Aung <rawaiucsm@gmail.com>
2023-03-04 21:53:46 +0630

    add mutation for user

--- src/index.js
+++ src/index.js
@@ -1,33 +1,110 @@
 // import { GraphqlServer } from "graphql-yoga";
 import { GraphQLServer } from "graphql-yoga";
+import { randomBytes, randomUUID } from "crypto";
 const users = [
   {
-    id: "13asdf",
-    name: "mon",
+    id: "1",
+    name: "peter",
     age: 26,
   },
   {
-    id: "12asdf",
-    name: "yimon",
+    id: "2",
+    name: "john doe",
     age: 24,
   },
   {
-    id: "123asdf",
-    name: "rawaiaung",
+    id: "3",
+    name: "smith",
     age: 28,
+  },
+];
+
+const posts = [
+  {
+    id: "10",
+    title: "Learn graphql",
+    body: "lorem",
+    published: false,
+    author: "1",
+  },
+  {
+    id: "11",
+    title: "Learn graphql for shopping",
+    body: "lorem for shopping",
+    published: false,
+    author: "2",
+  },
+  {
+    id: "12",
+    title: "Learn graphql for projects",
+    body: "lorem for project",
+    published: false,
+    author: "3",
+  },
+];
+
+const comments = [
+  {
+    id: "101",
+    text: "Your first comment",
+    author: "1",
+    post: "10",
+  },
+  {
+    id: "102",
+    text: "Your second comment",
+    author: "2",
+    post: "11",
+  },
+  {
+    id: "103",
+    text: "Your third comment",
+    author: "3",
+    post: "12",
+  },
+  {
+    id: "104",
+    text: "Your first comment",
+    author: "1",
+    post: "10",
   },
 ];
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
         grades: [Int!]!
         add(numbers: [Float!]!) : Float!
+    }
+
+    type Mutation {
+      createUser(name: String!,email: String,age: Int): User!
     }
 
     type User {
       id: ID!
       name: String!
-      age: Int!
+      email: String
+      age: Int
+      posts: [Post!]!
+      comments: [Comment!]!
+    }
+
+    type Post {
+      id: ID!
+      title: String!
+      body: String!
+      published: Boolean
+      author: User!
+      comments: [Comment!]!
+    }
+
+    type Comment {
+      id: ID!
+      text: String!
+      author: User!
+      post: Post!
     }
 `;
 
@@ -41,6 +118,20 @@ const resolvers = {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
+
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter((post) => {
+        return post.title.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+
+    comments(parent, args, ctx, info) {
+      return comments;
+    },
+
     add(parent, args, ctx, info) {
       if (args.numbers.length === 0) {
         return 0;
@@ -49,8 +140,64 @@ const resolvers = {
         return accumulator + currentValue;
       });
     },
+
     grades() {
       return [99, 100];
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const isEmail = users.some((user) => user.email === args.email);
+      if (isEmail) {
+        throw new Error("Email already taken");
+      }
+      const createdUser = {
+        id: randomBytes(4).toString("hex"),
+        name: args.name,
+        email: args.email,
+        age: args.agg,
+      };
+
+      users.push(createdUser);
+      return createdUser;
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
+  },
+
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post;
+      });
+    },
+  },
+
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
+      });
     },
   },
 };
